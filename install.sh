0e934786b0dc3cf3871ecb574ab492eeacf6d67d
ripps818 <ripps818@gmail.com>
2024-10-06 01:45:04 -0500

    fix install.sh and fix servce file

--- install.sh
+++ install.sh
@@ -3,17 +3,23 @@
 # Variables
 INSTALL_DIR="$HOME/.local/share/LockSSID"
 CONF_DIR="$HOME/.config/LockSSID"
+SYSTEMD_DIR="$HOME/.config/systemd/user"
 
-# Create necessary directories
-mkdir -p "$INSTALL_DIR"
-mkdir -p "$CONF_DIR"
+mkdir -p "$INSTALL_DIR" && echo "Install directory created: $INSTALL_DIR"
+mkdir -p "$CONF_DIR" && echo "Config directory created: $CONF_DIR"
+mkdir -p "$SYSTEMD_DIR" && echo "Config directory created: $SYSTEMD_DIR"
 
-# Copy scripts to the install directory
-cp "$0" "$INSTALL_DIR/monitor-wifi-lock.sh"
-cp "$INSTALL_DIR/lock-wifi.sh" "$INSTALL_DIR/unlock-wifi.sh" "$INSTALL_DIR/utils.sh" "$CONF_DIR/lockssid.conf"
+# Install LockSSID scripts and configuration
+install -m 755 lock-wifi.sh unlock-wifi.sh monitor-wifi-lock.sh utils.sh "$INSTALL_DIR/"
+install -m 644 lockssid.conf "$CONF_DIR/"
 
-# Set up systemd service
-cp "$INSTALL_DIR/LockSSID.service" "$HOME/.config/systemd/user/LockSSID.service"
+# Install the systemd service file
+install -m 644 LockSSID.service ~/.config/systemd/user/
+if [ ! -f "$SYSTEMD_DIR/LockSSID.service" ]; then
+  echo "Error: $SYSTEMD_DIR/LockSSID.service not found."
+  exit 1
+fi
+
 systemctl --user enable LockSSID.service
 systemctl --user start LockSSID.service
 
