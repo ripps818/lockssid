09d4ed5901dc83c5e3fb9db309c828f15c2c1d4d
ripps818 <ripps818@gmail.com>
2025-01-07 22:08:21 -0600

    Update unlock-wifi.sh
    enable power_save when unlocking

--- unlock-wifi.sh
+++ unlock-wifi.sh
@@ -3,7 +3,6 @@
 # Load utility functions
 source "$HOME/.local/share/LockSSID/utils.sh"
 
-
 # Get the current SSID
 SSID=$(get_current_ssid)
 
@@ -11,6 +10,16 @@ if [ -z "$SSID" ]; then
     log_message "No active Wi-Fi connection found. Exiting."
     exit 1
 fi
+
+# Get the wifi card's device name
+DEVICE_NAME=$(iw dev | grep Interface | awk '{print $2}')
+
+if [ -z "$DEVICE_NAME" ]; then
+    log_message "No wireless device found. Exiting."
+    exit 1
+fi
+# Turn off power saving mode
+iw dev "$DEVICE_NAME" set power_save on
 
 # Unlocking logic
 log_message "Unlocking Wi-Fi for SSID: $SSID"
