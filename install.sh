afd996d05c3bed136f43fb300bf6ccddd4d3fe28
ripps818 <ripps818@gmail.com>
2025-01-07 22:15:50 -0600

    Update install.sh
    Install to /etc/lockssid now, since we are no longer able to run this as user, since we need some programs that run as root in order to function

--- install.sh
+++ install.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
 # Variables
-INSTALL_DIR="$HOME/.local/share/LockSSID"
-CONF_DIR="$HOME/.config/LockSSID"
-SYSTEMD_DIR="$HOME/.config/systemd/user"
+INSTALL_DIR="/etc/lockssid"
+SYSTEMD_DIR="/etc/systemd/system"
 
-mkdir -p "$INSTALL_DIR" && echo "Install directory created: $INSTALL_DIR"
-mkdir -p "$CONF_DIR" && echo "Config directory created: $CONF_DIR"
-mkdir -p "$SYSTEMD_DIR" && echo "Config directory created: $SYSTEMD_DIR"
+sudo mkdir -p "$INSTALL_DIR" && echo "Install directory created: $INSTALL_DIR"
 
 # Check if the config file already exists
-if [[ -f "$CONF_DIR/lockssid.conf" ]]; then
+if [[ -f "$INSTALL_DIR/lockssid.conf" ]]; then
     read -p "Configuration file already exists. Do you want to overwrite it? (y/n): " overwrite
     if [[ "$overwrite" != "y" ]]; then
         echo "Skipping configuration file overwrite."
     else
         echo "Overwriting configuration file."
-        install -m 644 lockssid.conf "$CONF_DIR/"
+        sudo install -m 644 lockssid.conf "$INSTALL_DIR/"
     fi
 else
     echo "Installing configuration file."
-    install -m 644 lockssid.conf "$CONF_DIR/"
+    sudo install -m 644 lockssid.conf "$INSTALL_DIR/"
 fi
 
 # Install LockSSID scripts
-install -m 755 lock-wifi.sh unlock-wifi.sh monitor-wifi-lock.sh utils.sh "$INSTALL_DIR/"
+sudo install -m 755 lock-wifi.sh unlock-wifi.sh monitor-wifi-lock.sh utils.sh "$INSTALL_DIR/"
+
+# Ensure scripts have execute permissions
+sudo chmod +x "$INSTALL_DIR/lock-wifi.sh"
+sudo chmod +x "$INSTALL_DIR/unlock-wifi.sh"
+sudo chmod +x "$INSTALL_DIR/monitor-wifi-lock.sh"
+sudo chmod +x "$INSTALL_DIR/utils.sh"
 
 # Install the systemd service file
-install -m 644 LockSSID.service "$SYSTEMD_DIR/"
+sudo install -m 644 LockSSID.service "$SYSTEMD_DIR/"
 if [ ! -f "$SYSTEMD_DIR/LockSSID.service" ]; then
   echo "Error: $SYSTEMD_DIR/LockSSID.service not found."
   exit 1
 fi
 
-systemctl --user enable LockSSID.service
-systemctl --user start LockSSID.service
+sudo systemctl enable lockssid.service
+sudo systemctl start lockssid.service
 
 echo "LockSSID installed successfully."
