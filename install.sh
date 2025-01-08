b171b1ba975e714973002c8baa45a68c57eb2bfc
ripps818 <ripps818@gmail.com>
2025-01-07 22:32:34 -0600

    Update install.sh
    Lets not install lockssid if we call the --remove-old flag

--- install.sh
+++ install.sh
@@ -7,7 +7,15 @@ SERVICE_FILE="lockssid.service"
 OLD_INSTALL_DIR="$HOME/.local/share/LockSSID"
 OLD_CONF_DIR="$HOME/.config/LockSSID"
 OLD_SERVICE_FILE="LockSSID.service"
-REMOVE_OLD=false
+
+# Function to uninstall LockSSID
+uninstall_lockssid() {
+    sudo systemctl stop "$SERVICE_FILE"
+    sudo systemctl disable "$SERVICE_FILE"
+    sudo rm -f "$SYSTEMD_DIR/$SERVICE_FILE"
+    sudo rm -rf "$INSTALL_DIR"
+    echo "LockSSID uninstalled successfully."
+}
 
 # Function to remove old user configs and folders
 remove_old_configs() {
@@ -27,10 +35,6 @@ remove_old_configs() {
 
 # Function to install LockSSID
 install_lockssid() {
-    if [ "$REMOVE_OLD" = true ]; then
-        remove_old_configs
-    fi
-
     sudo mkdir -p "$INSTALL_DIR" && echo "Install directory created: $INSTALL_DIR"
 
     # Check if the config file already exists
@@ -69,15 +73,6 @@ install_lockssid() {
     echo "LockSSID installed successfully."
 }
 
-# Function to uninstall LockSSID
-uninstall_lockssid() {
-    sudo systemctl stop "$SERVICE_FILE"
-    sudo systemctl disable "$SERVICE_FILE"
-    sudo rm -f "$SYSTEMD_DIR/$SERVICE_FILE"
-    sudo rm -rf "$INSTALL_DIR"
-    echo "LockSSID uninstalled successfully."
-}
-
 # Parse flags
 for arg in "$@"; do
     case $arg in
@@ -86,7 +81,8 @@ for arg in "$@"; do
         exit 0
         ;;
         --remove-old)
-        REMOVE_OLD=true
+        remove_old_configs
+        exit 0
         ;;
     esac
 done
