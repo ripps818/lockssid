afe82525b01a2a0b1a2394456f3d8decd01596aa
ripps818 <ripps818@gmail.com>
2025-01-07 22:44:58 -0600

    Update unlock-wifi.sh
    - source correct utils.sh
    - set managed to yes when unlocking

--- unlock-wifi.sh
+++ unlock-wifi.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!#!/bin/bash
 
 # Load utility functions
-source "$HOME/.local/share/LockSSID/utils.sh"
+source "/etc/lockssid/utils.sh"
 
 # Get the current SSID
 SSID=$(get_current_ssid)
@@ -18,12 +18,16 @@ if [ -z "$DEVICE_NAME" ]; then
     log_message "No wireless device found. Exiting."
     exit 1
 fi
-# Turn off power saving mode
-iw dev "$DEVICE_NAME" set power_save on
 
 # Unlocking logic
 log_message "Unlocking Wi-Fi for SSID: $SSID"
 nmcli con mod "$SSID" 802-11-wireless.bssid ''
+
+# Re-enable NetworkManager scanning for other networks
+nmcli dev set "$DEVICE_NAME" managed yes
+
+# Turn on power saving mode
+iw dev "$DEVICE_NAME" set power_save on
 
 # Optionally disconnect and reconnect
 nmcli device disconnect "$SSID"
